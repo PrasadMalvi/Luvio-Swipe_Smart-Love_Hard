e2977281ed19e0b0b5c146f3a602685083d51822
Prasad Malvi <prasadmalvi23@gmail.com>
2025-04-09 23:11:40 +0530

    Added Location and distance and working on Explore

--- luvio-Backend/Models/FaceData.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const FaceDataSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  faceDescriptors: { type: Array, required: true },
-});
-
-module.exports = mongoose.model("FaceData", FaceDataSchema);
--- luvio-webapp/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}", // Include your file paths
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [require("tailwind-scrollbar-hide")],
-};
